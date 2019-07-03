065fb00f52982284e7f8c324d7710b42c7e0bf14
rblood79 <rblood79@gmail.com>
2019-07-04 02:50:34 +0900

    ss

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3e3a8fd226a90ed70e9ed0958529ae7c.js"
+  "/precache-manifest.98aa2542131f5393fb211ef0175f6395.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "haemilsoft-vue"});
